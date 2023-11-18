6e73e4d0c23e7bb5789379a7777796dcecd1eb8f
20120602 <nmt762cva@gmail.com>
2023-11-18 15:45:37 +0700

    fix header, footer

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,15 +1,47 @@
-// Footer.js
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import { Container, Typography, Grid, List, ListItemButton, ListItemText, AppBar, Toolbar } from '@mui/material';
 
 const Footer = () => {
     return (
         <AppBar position="static" color="primary">
-            <Container maxWidth="md">
+            <Container>
                 <Toolbar>
-                    <Typography variant="body2" color="inherit">
-                        © 2023 FIT - HCMUS. All rights reserved.
-                    </Typography>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center" p={2}>
+                        {/* Cột 1: Danh sách các mục trên navbar */}
+                        <Grid item xs={12} md={8}>
+                            <Typography variant="h6" gutterBottom>
+                                FIT - HCMUS
+                            </Typography>
+                            <List>
+                                <ListItemButton href='/home'>
+                                    <ListItemText primary="Home" />
+                                </ListItemButton>
+                                <ListItemButton href='/signin'>
+                                    <ListItemText primary="About" />
+                                </ListItemButton>
+                                <ListItemButton href='/signup'>
+                                    <ListItemText primary="Services" />
+                                </ListItemButton>
+                                {/* Thêm các mục khác theo cần thiết */}
+                            </List>
+                        </Grid>
+
+                        {/* Cột 2: Thông tin liên hệ */}
+                        <Grid item xs={12} md={4}>
+                            <Typography variant="h6" gutterBottom>
+                                Contact Information
+                            </Typography>
+                            <Typography variant="body1">
+                                Address: District 5, HCM city, Vietnam
+                            </Typography>
+                            <Typography variant="body1">
+                                Phone: (123) 456-7890
+                            </Typography>
+                            <Typography variant="body1">
+                                Email: info@example.com
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </Container>
         </AppBar>
--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,40 +1,138 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Typography, Container, Box } from '@mui/material';
 
-const Header = ({ isLoggedIn, handleSignOut }) => (
-    <AppBar position="static" color="primary">
-        <Container maxWidth="md">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Button color="inherit" component={Link} to={isLoggedIn ? '/home' : '/'}>
-                        Home
-                    </Button>
-                </Typography>
-                <Box sx={{ display: 'flex', gap: 2 }}>
-                    {isLoggedIn ? (
-                        <>
-                            <Button color="inherit" component={Link} to="/profile">
-                                Profile
+const pages = ['Home', 'About', 'Service'];
+
+function Header() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">
+                                        <Link style={{ textDecoration: 'none', color: 'black' }} to={`/${page}`}>
+                                            {page}
+                                        </Link>
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
+                                <Link style={{ textDecoration: 'none', color: 'white' }} to={`/${page}`}>
+                                    {page}
+                                </Link>
                             </Button>
-                            <Button color="inherit" onClick={handleSignOut}>
-                                Sign Out
-                            </Button>
-                        </>
-                    ) : (
-                        <>
-                            <Button color="inherit" component={Link} to="/signin">
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Button variant="outlined" color="inherit" sx={{ mx: 2 }}>
+                            <Link to={'/signin'} style={{ textDecoration: 'none', color: 'white' }}>
                                 Sign In
-                            </Button>
-                            <Button color="inherit" component={Link} to="/signup">
+                            </Link>
+                        </Button>
+                        <Button variant="outlined" color="inherit" sx={{ mx: 2 }}>
+                            <Link to={'/signup'} style={{ textDecoration: 'none', color: 'white' }}>
                                 Sign Up
-                            </Button>
-                        </>
-                    )}
-                </Box>
-            </Toolbar>
-        </Container>
-    </AppBar>
-);
+                            </Link>
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
 
 export default Header;
