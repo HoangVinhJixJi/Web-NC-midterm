4675a483622053e033e0c1dc6885c9175fe68ac5
20120602 <nmt762cva@gmail.com>
2023-11-19 22:02:45 +0700

    fix signup confict

--- Frontend/src/components/SignUp.js
+++ Frontend/src/components/SignUp.js
@@ -139,13 +139,8 @@ const SignUp = () => {
             fullWidth
             required
           >
-<<<<<<< HEAD
             <MenuItem value="Male">Male</MenuItem>
             <MenuItem value="Female">Female</MenuItem>
-=======
-            <MenuItem value="Nam">Male</MenuItem>
-            <MenuItem value="Nữ">Female</MenuItem>
->>>>>>> 7845a02e32e83cc75b3ae2b45ba8f0f3c9da2079
           </TextField>
           <TextField
             label="Email"
