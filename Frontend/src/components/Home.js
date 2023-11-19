8a1b0b8e3d5bbbd808844ecfd6f3fb5744781e0e
20120602 <nmt762cva@gmail.com>
2023-11-19 18:04:02 +0700

    add note homepage

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,20 +1,70 @@
-import React, { useEffect } from 'react';
-import { Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Typography,
+  Button,
+  Container,
+  Paper,
+  Grid,
+  TextField,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
 import { useAuth as useAuthContext } from '../api/AuthContext';
 
 function Home() {
   const { isLoggedIn } = useAuthContext();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    // Kiểm tra trạng thái đăng nhập
-    if (!isLoggedIn) {
-      // Nếu chưa đăng nhập, điều hướng về trang Landing
-      navigate('/');
-    }
-  }, [isLoggedIn, navigate]);
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState({ title: '', content: '', color: '#ffffff' });
+  const [editIndex, setEditIndex] = useState(null);
+  const [colorPickerAnchor, setColorPickerAnchor] = useState(null);
+
+  const handleAddNote = () => {
+    setNotes([...notes, { ...newNote, timestamp: new Date() }]);
+    setNewNote({ title: '', content: '', color: '#ffffff' });
+    setColorPickerAnchor(null);
+  };
+
+  const handleEditNote = (index) => () => {
+    setEditIndex(index);
+    setNewNote({ ...notes[index] });
+    setColorPickerAnchor(null);
+  };
+
+  const handleSaveNote = () => {
+    const updatedNotes = [...notes];
+    updatedNotes[editIndex] = { ...newNote, timestamp: new Date() };
+    setNotes(updatedNotes);
+    setNewNote({ title: '', content: '', color: '#ffffff' });
+    setEditIndex(null);
+    setColorPickerAnchor(null);
+  };
+
+  const handleDeleteNote = (index) => () => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+    setColorPickerAnchor(null);
+  };
+
+  const handleColorPickerClick = (event) => {
+    setColorPickerAnchor(event.currentTarget);
+  };
+
+  const handleColorPickerClose = () => {
+    setColorPickerAnchor(null);
+  };
+
+  const handleColorSelect = (color) => {
+    setNewNote({ ...newNote, color });
+    setColorPickerAnchor(null);
+  };
 
   return (
     <div>
@@ -52,6 +102,83 @@ function Home() {
       <Button variant="contained" color="primary" component={Link} to="/users/profile" style={{ marginTop: '20px' }}>
         Update Profile
       </Button>
+
+      {/* Ghi chú */}
+      <Typography variant="h4" mt={4} mb={2}>
+        Your Notes
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} sx={{ p: 3, minHeight: '100%' }}>
+            <TextField
+              fullWidth
+              label="Note Title"
+              variant="outlined"
+              margin="normal"
+              value={newNote.title}
+              onChange={(e) => setNewNote({ ...newNote, title: e.target.value })}
+            />
+            <TextField
+              fullWidth
+              multiline
+              rows={4}
+              label="Note Content"
+              variant="outlined"
+              margin="normal"
+              value={newNote.content}
+              onChange={(e) => setNewNote({ ...newNote, content: e.target.value })}
+              sx={{ overflowWrap: 'break-word' }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleColorPickerClick}
+              startIcon={<ColorLensIcon />}
+              sx={{ mt: 2, mr: 2 }}
+            >
+              Choose Color
+            </Button>
+            <Menu
+              anchorEl={colorPickerAnchor}
+              open={Boolean(colorPickerAnchor)}
+              onClose={handleColorPickerClose}
+            >
+              <MenuItem onClick={() => handleColorSelect('#ffffff')}>White</MenuItem>
+              <MenuItem onClick={() => handleColorSelect('#ffcccb')}>Light Red</MenuItem>
+              <MenuItem onClick={() => handleColorSelect('#c1ffc1')}>Light Green</MenuItem>
+              {/* Thêm các màu khác tại đây */}
+            </Menu>
+            {editIndex !== null ? (
+              <Button variant="contained" color="primary" onClick={handleSaveNote} sx={{ mt: 2 }}>
+                Save Note
+              </Button>
+            ) : (
+              <Button variant="contained" color="primary" onClick={handleAddNote} sx={{ mt: 2 }}>
+                Add Note
+              </Button>
+            )}
+          </Paper>
+        </Grid>
+        {notes.map((note, index) => (
+          <Grid item xs={12} md={6} key={index}>
+            <Paper elevation={3} sx={{ p: 3, position: 'relative', height: '100%', backgroundColor: note.color, minHeight: '100%' }}>
+              <Typography variant="h6">{note.title}</Typography>
+              <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
+                {note.timestamp.toLocaleString()}
+              </Typography>
+              <Typography variant="body1" sx={{ mt: 2, mb: 2, whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>{note.content}</Typography>
+              <div style={{ position: 'absolute', bottom: '8px', right: '8px'}}>
+                <IconButton color="primary" aria-label="edit" onClick={handleEditNote(index)}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton color="primary" aria-label="delete" onClick={handleDeleteNote(index)} sx={{ ml: 1 }}>
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
--- Frontend/src/components/Landing.js
+++ Frontend/src/components/Landing.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Typography, Button, Container, Paper } from '@mui/material';
+import React from 'react';
+import { Typography, Button, Container, Paper, Grid } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { useAuth as useAuthContext } from '../api/AuthContext';
@@ -8,29 +8,55 @@ const Landing = () => {
   const { isLoggedIn } = useAuthContext();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    // Kiểm tra trạng thái đăng nhập
-    if (isLoggedIn) {
-      // Nếu đã đăng nhập, điều hướng về trang Home
-      navigate('/home');
-    }
-  }, [isLoggedIn, navigate]);
+  // Nếu đã đăng nhập, điều hướng về trang Home
+  if (isLoggedIn) {
+    navigate('/home');
+    return null; // Tránh hiển thị nội dung của trang Landing nếu đã đăng nhập
+  }
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="lg">
       <Paper elevation={3} sx={{ padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h4" gutterBottom>
-          Welcome to Our App
+        <Typography variant="h2" gutterBottom>
+          Welcome to Our Amazing App
         </Typography>
         <Typography variant="body1" paragraph>
-          This is the landing page of our application. Please sign in to access the full features.
+          Discover the incredible features that await you. Sign in now!
         </Typography>
-        <Button variant="contained" color="primary" component={Link} to="/signin">
+        <Button variant="contained" color="primary" size="large" component={Link} to="/signin">
           Sign In
         </Button>
         <Typography variant="body2" mt={2}>
           Don't have an account? <Link to="/signup">Sign Up</Link>
         </Typography>
+
+        {/* Nội dung thêm */}
+        <Grid container spacing={3} mt={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper elevation={3} sx={{ p: 3, textAlign: 'center', height: '100%' }}>
+              <Typography variant="h4">Explore Features</Typography>
+              <Typography variant="body2" mt={2}>
+                Discover a variety of features and functionalities designed to enhance your experience.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper elevation={3} sx={{ p: 3, textAlign: 'center', height: '100%' }}>
+              <Typography variant="h4">Connect with Others</Typography>
+              <Typography variant="body2" mt={2}>
+                Join a vibrant community and connect with other users who share your interests.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper elevation={3} sx={{ p: 3, textAlign: 'center', height: '100%' }}>
+              <Typography variant="h4">Customize Your Profile</Typography>
+              <Typography variant="body2" mt={2}>
+                Personalize your profile to make this platform truly yours.
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
       </Paper>
     </Container>
   );
